457fee9cd4151d749aa5b3d3e1e19981c8be920b
sunqi <1766649908@qq.com>
2017-05-18 02:49:15 +0800

    [About] 完成"关于"界面，重新选定主logo

--- app/src/main/java/com/example/sunqi/securityking/ui/AboutActivity.java
+++ app/src/main/java/com/example/sunqi/securityking/ui/AboutActivity.java
@@ -1,15 +1,52 @@
 package com.example.sunqi.securityking.ui;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.sunqi.securityking.R;
 
 /**
+ * 系统介绍
  * Created by sunqi on 2017/5/17.
  */
 
 public class AboutActivity extends Activity{
+
+    private View includeView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setStatusBarTranslate();
+        setContentView(R.layout.activity_about);
+        includeView = findViewById(R.id.about_top_layout);
+        if (includeView != null) {
+            ((ImageView)includeView.findViewById(R.id.normal_title_back)).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+            ((TextView)includeView.findViewById(R.id.normal_title_text)).setText("关于");
+            ((ImageView)includeView.findViewById(R.id.title_items)).setVisibility(View.GONE);
+        }
+    }
+
+    private void setStatusBarTranslate(){
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 }
