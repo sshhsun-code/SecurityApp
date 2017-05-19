5da2d0d572d767888b2ad8d221ec9173d50c1d87
sunqi <1766649908@qq.com>
2017-05-20 03:08:37 +0800

    [AntiVirus] 得到病毒扫描结果，并展示在界面并完善展示,点击逻辑

--- app/src/main/java/com/example/sunqi/securityking/ui/ScanVirusActicity.java
+++ app/src/main/java/com/example/sunqi/securityking/ui/ScanVirusActicity.java
@@ -3,13 +3,24 @@ package com.example.sunqi.securityking.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Message;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.sunqi.securityking.R;
@@ -20,18 +31,15 @@ import com.example.sunqi.securityking.customview.RadarScanView;
 import com.example.sunqi.securityking.dataprovider.VirusScanDataProcessor;
 import com.example.sunqi.securityking.xmlparser.PullParseService;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.example.sunqi.securityking.R.drawable.manager;
 
 /**
  * Created by sunqi on 2017/5/16.
  */
 
-public class ScanVirusActicity extends Activity implements VirusScanDataProcessor.ScanListener{
+public class ScanVirusActicity extends Activity implements VirusScanDataProcessor.ScanListener, View.OnClickListener{
 
     private ArrayList<String> apps = new ArrayList<>();
     private List<VirusShowBean> mVirusShowBeanList = new ArrayList<>();
@@ -39,10 +47,19 @@ public class ScanVirusActicity extends Activity implements VirusScanDataProcesso
     private RadarScanView racarscanview;
     private TextView scan_state;
     private TextView app_name;
+    private TextView normal_title_text;
+    private View scan_title;
+    private RelativeLayout scan_view;
+    private RelativeLayout result_view;
+    private RelativeLayout normal_title_layout;
+    private ImageView normal_title_back;
+    private ImageView title_items;
+    private ListView result_listview;
 
     private Handler mhandler;
     private HandlerThread scanThread;
     private Handler scanHandler;
+    private VirusAdapter adapter;
 
     private boolean isScanning = true;
 
@@ -56,12 +73,37 @@ public class ScanVirusActicity extends Activity implements VirusScanDataProcesso
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setStatusBarTranslate();
         setContentView(R.layout.activity_scan_virus);
         initData();
         initView();
     }
 
+    private void setStatusBarTranslate() {
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
     private void initView() {
+        scan_title = findViewById(R.id.scan_title);
+        normal_title_text = (TextView) scan_title.findViewById(R.id.normal_title_text);
+        normal_title_text.setText("病毒扫描");
+        scan_view = (RelativeLayout) findViewById(R.id.scan_view);
+        scan_view.setVisibility(View.VISIBLE);
+        result_view = (RelativeLayout) findViewById(R.id.result_view);
+        result_view.setVisibility(View.GONE);
+        scan_title.setBackgroundResource(R.color.cn_rank_protect_bg_blue);
+        normal_title_back = (ImageView) scan_title.findViewById(R.id.normal_title_back);
+        normal_title_back.setOnClickListener(this);
+        title_items = (ImageView) scan_title.findViewById(R.id.title_items);
+        title_items.setVisibility(View.GONE);
         racarscanview = (RadarScanView) findViewById(R.id.racarscanview);
         scan_state = (TextView) findViewById(R.id.scan_state);
         app_name = (TextView) findViewById(R.id.app_name);
@@ -77,6 +119,15 @@ public class ScanVirusActicity extends Activity implements VirusScanDataProcesso
         }
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.normal_title_back:
+                finish();
+                break;
+        }
+    }
+
     private void getToListActicity() {
         racarscanview.stopScan();
         app_name.setVisibility(View.GONE);
@@ -86,7 +137,23 @@ public class ScanVirusActicity extends Activity implements VirusScanDataProcesso
             @Override
             public void run() {
                 if (hasVirusApp) {
-
+                    scan_view.setVisibility(View.GONE);
+                    result_view.setVisibility(View.VISIBLE);
+                    findViewById(R.id.total_layout).setBackgroundColor(Color.WHITE);
+                    scan_title.setBackgroundResource(R.color.cn_rank_protect_bg_red);
+                    result_listview = (ListView) findViewById(R.id.result_listview);
+                    adapter = new VirusAdapter(mVirusShowBeanList);
+                    result_listview.setAdapter(adapter);
+                    result_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            String pkgName = mVirusShowBeanList.get(position).getPakName();
+                            Intent intent = new Intent();
+                            intent.setAction(Intent.ACTION_DELETE);
+                            intent.setData(Uri.parse("package:" + pkgName));
+                            startActivity(intent);
+                        }
+                    });
                 }
             }
         },2000);
@@ -183,5 +250,54 @@ public class ScanVirusActicity extends Activity implements VirusScanDataProcesso
         scanThread.quit();
 
         index = 0;
+    }
+
+    static class VirusAdapter extends BaseAdapter {
+        private List<VirusShowBean> virusApps;
+
+        public VirusAdapter(List<VirusShowBean> virusApps) {
+            this.virusApps = virusApps;
+        }
+
+        @Override
+        public int getCount() {
+            return virusApps.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return virusApps.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            VirusShowBean virusShowBean = virusApps.get(position);
+            if (convertView == null) {
+                convertView = LayoutInflater.from(SecurityApplication.getInstance()).inflate(R.layout.virus_app_item, null);
+            }
+
+            ((ImageView)convertView.findViewById(R.id.virus_item_icon)).setImageDrawable(virusShowBean.getAppIcon());
+            ((TextView)convertView.findViewById(R.id.virus_item_name)).setText(virusShowBean.getAppName());
+            ((TextView)convertView.findViewById(R.id.virus_item_type)).setText(getVirusType(virusShowBean.getType()));
+            return convertView;
+        }
+
+        private String getVirusType(int type) {
+            String typeString = "";
+            switch (type) {
+                case 1:
+                    typeString = "含有高危病毒";
+                    break;
+                case 2:
+                    typeString = "该应用频繁后台操作";
+                    break;
+            }
+            return typeString;
+        }
     }
 }
